de3b384cd1c17dd9c18cc2d47de1eef4ab768aee
Quentin Warnant <lequentin@gmail.com>
2023-11-05 17:42:59 +0000

    Adding Bilinear interpolation function and use it for Velocity & Die Advection, adding DeltaTime (fixed) to advection. and adding DieField copy to avoid conflicts

--- FluidSim.py
+++ FluidSim.py
@@ -6,7 +6,8 @@ import sys
 
 ti.init(arch=ti.gpu)
 
-dim = 320
+dt = 16.6666e-3
+dim = 420
 VelocityField = ti.Vector.field(n=2, dtype=float, shape=(dim, dim))
 VelocityField_Old = ti.Vector.field(n=2, dtype=float, shape=(dim, dim))
 DebugVelocityField = ti.Vector.field(n=2, dtype=float, shape=(dim, dim))
@@ -17,6 +18,7 @@ PressureField_Old = ti.field(ti.f32, shape=(dim, dim))
 DebugPressureField = ti.field(ti.f32, shape=(dim,dim))
 
 DieField = ti.field(ti.f32, shape=(dim,dim))
+DieField_Old = ti.field(ti.f32, shape=(dim,dim))
 
 @ti.func
 def ResetFields():
@@ -41,6 +43,17 @@ def ReadInput(gui : ti.GUI, PrevFrameCursorPos : tm.vec2):
     CursorVelocity = (CursorPos - PrevFrameCursorPos)
     return CursorPos, CursorVelocity
 
+@ti.func
+def BilinearInterpolate(Coord, SampledField): 
+    BL = SampledField[ tm.clamp( int(tm.floor( Coord.x)), 0, dim-1), tm.clamp( int(tm.floor( Coord.y)), 0, dim-1)]
+    TL = SampledField[ tm.clamp( int(tm.floor( Coord.x)), 0, dim-1), tm.clamp( int(tm.ceil( Coord.y)), 0, dim-1)]
+    BR = SampledField[ tm.clamp( int(tm.floor( Coord.x)), 0, dim-1), tm.clamp( int(tm.floor( Coord.y)), 0, dim-1)]
+    TR = SampledField[ tm.clamp( int(tm.ceil( Coord.x)), 0, dim-1), tm.clamp( int(tm.ceil( Coord.y)), 0, dim-1)]
+
+    HoriBottom = tm.mix(BL, BR, tm.fract(Coord.x))
+    HoriTop = tm.mix(TL, TR, tm.fract(Coord.x))
+    return tm.mix(HoriBottom, HoriTop, tm.fract(Coord.y))
+
 @ti.kernel
 def Init():
     ResetFields()
@@ -53,20 +66,19 @@ velocityStampField = ti.field(ti.i16, shape=(20,20))
 @ti.kernel
 def AddInputVelocity(Pos: tm.vec2, Velocity : tm.vec2):
     for i,j in velocityStampField:
-        VelocityField[tm.clamp(int(Pos[0] * dim) + (i-10), 0, dim), tm.clamp(int(Pos[1] * dim) + (j-10), 0, dim)] += Velocity * 30.
+        VelocityField[tm.clamp(int(Pos.x * dim) + (i-10), 0, dim-1), tm.clamp(int(Pos.y * dim) + (j-10), 0, dim-1)] += Velocity * 300.
     
 @ti.kernel
 def AdvectVelocity():
     for i, j in VelocityField:  # Parallelized over all pixels
-        CurrCellVel = VelocityField[i,j]
-        VelocityField[i,j] = VelocityField_Old[tm.clamp( i-int(CurrCellVel.x), 0, dim), tm.clamp(j-int(CurrCellVel.y), 0, dim)] #should do interpolation
-
+        CurrCellVel = VelocityField_Old[i,j]
+        VelocityField[i,j] = BilinearInterpolate( tm.vec2(float(i) - (dim * CurrCellVel.x * dt), float(j) - (dim * CurrCellVel.y * dt) ), VelocityField_Old)
 
 @ti.kernel
 def AdvectDie():
     for i, j in VelocityField:  # Parallelized over all pixels
         CurrCellVel = VelocityField[i,j]
-        DieField[i,j] = DieField[tm.clamp( i-int(CurrCellVel.x), 0, dim), tm.clamp(j-int(CurrCellVel.y), 0, dim)] #should do interpolation
+        DieField[i,j] = BilinearInterpolate( tm.vec2(float(i) - (dim * CurrCellVel.x * dt), float(j) - (dim * CurrCellVel.y * dt)) , DieField_Old) 
 
 @ti.kernel
 def CalculateDivergence():
@@ -149,7 +161,6 @@ JacobiIterationCount = 60
 DisplayedBuffer = 0
 
 while gui.running:
-
     #Keyboard input (espace to close, space to reset, A/Z/E/R to swap between buffer visualisation)
     if gui.get_event(ti.GUI.RELEASE):
         if gui.event.key == ti.GUI.ESCAPE:
@@ -166,13 +177,10 @@ while gui.running:
         if  gui.event.key == 'r':
             DisplayedBuffer = 3
 
-    print(DisplayedBuffer)
-
     PrevFrameCursorPos, Velocity = ReadInput(gui, PrevFrameCursorPos)
     AddInputVelocity(PrevFrameCursorPos, Velocity)
     EnforceBoundaryConditions_Velocity()
-
-    #print(VelocityField)
+    
     VelocityField_Old.copy_from(VelocityField)
     AdvectVelocity()
     EnforceBoundaryConditions_Velocity()
@@ -184,32 +192,33 @@ while gui.running:
     #AddExternalForces()
     #EnforceBoundaryConditions_Velocity()
 
-
     #Calculate divergence from velocity field
     CalculateDivergence()
 
     PressureField.fill(0)
     PressureField_Old.fill(0)
     #Solve Pressure
-    for i in range(JacobiIterationCount):
+    for i in range(JacobiIterationCount): 
         ComputePressure()
         EnforceBoundaryConditions_Pressure()
         lastIteration = (i == JacobiIterationCount-1) 
          #copy new to old - for next iteration
         if not lastIteration:
-            PressureField_Old.copy_from(PressureField)
+            PressureField_Old.copy_from(PressureField) #TODO: Ping pong instead of copy
     #remove gradient of Pressure from velocity (ie: remove divergence)
     RemoveDivergenceFromVelocity()
 
-
+    DieField_Old.copy_from(DieField)
     AdvectDie()
     EnforceBoundaryConditions_Die()
+
 
     match DisplayedBuffer:
         case 0:
             gui.set_image(DieField)
         case 1:
             GenerateDebugVelocityField()
+            #gui.vector_field(VelocityField)
             gui.set_image(DebugVelocityField)
         case 2:
             gui.set_image(DivergenceField)

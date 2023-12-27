66d7994e41e52224b4c2356b507c748039e93b8b
Quentin Warnant <lequentin@gmail.com>
2023-12-27 21:38:07 +0000

    Solve 256x256 PDE with Jacobi , first pass

--- FluidSim/PoissonSolvers/JacobiSolver.py
+++ FluidSim/PoissonSolvers/JacobiSolver.py
@@ -4,9 +4,8 @@ import taichi.math as tm
 _DEBUG = False
 
 # Configuration
-dim = 4
-DimLvl1 = dim
-
+dim = 256
+dimSqr = dim * dim
 
 # Taichi Initialisation
 if _DEBUG:
@@ -17,41 +16,29 @@ else:
 # Buffer creation
 
 #Resolution level 1    
-VHField_Ping = ti.field(ti.f32, shape=(DimLvl1, DimLvl1))      # approximation of "u" (unknown) field at a given iteration
-VHField_Pong = ti.field(ti.f32, shape=(DimLvl1, DimLvl1))      # approximation of "u" (unknown) field at a given iteration
-FHField = ti.field(ti.f32, shape=(DimLvl1, DimLvl1))
-ResidualField = ti.field(ti.f32, shape=(DimLvl1, DimLvl1))
-
-# @ti.dataclass
-# class PoissonProblem:
-#     Size : tm.vec2
-#     def __init__(self, size) -> None:
-#         self.Size = size
+VHField_Ping = ti.field(ti.f32, shape=(dim, dim))      # approximation of "u" (unknown) field at a given iteration
+VHField_Pong = ti.field(ti.f32, shape=(dim, dim))      # approximation of "u" (unknown) field at a given iteration
+FHField = ti.field(ti.f32, shape=(dim, dim))
+ResidualField = ti.field(ti.f32, shape=(dim, dim))
 
 @ti.func
 def Reset():
     VHField_Ping.fill(0.)
     VHField_Pong.fill(0.)
-#    FHField.fill(0.)
-    FHField[0,0] = 7
-    FHField[0,1]= 10
-    FHField[0,2]= 13
-    FHField[0,3]= 16
 
-    FHField[1,0]= 20
-    FHField[1,1]= 24
-    FHField[1,2]= 28
-    FHField[1,3]= 32
+    FHField.fill(0.)
 
-    FHField[2,0]= 36
-    FHField[2,1]= 40
-    FHField[2,2]= 44
-    FHField[2,3]= 48
+    #b , F
+    for i in range(0,dim):
+        FHField[i,0] = 60.
+    for i in range (60, dim-60):   
+        for j in range(0,10):
+            FHField[i,dim-2-j] = -15.
+            FHField[j,i] = -15.
 
-    FHField[3,0]= 35
-    FHField[3,1]= 38
-    FHField[3,2]= 41
-    FHField[3,3]= 44
+    # for x,y in FHField:
+    #     FHField[x,y] = -5. * tm.exp(x) * tm.exp(-2. * y)
+
 
     ResidualField.fill(0.)
 
@@ -62,78 +49,101 @@ def Init():
 @ti.func
 def SampleField(f, coord : tm.vec2) -> ti.f32:
     extents = tm.vec2(f.shape[0]-1, f.shape[1]-1)
-    coord = tm.clamp(coord, tm.vec2(0,0), extents)
-    return f[ti.i32(coord.x), ti.i32(coord.y)]
+    #coord = tm.clamp(coord, tm.vec2(0,0), extents)
+    val = 0.
+    if coord.x < 0 or coord.x >= extents.x or coord.y < 0 or coord.y >= extents.y:
+        val = 0.
+    else:
+        val = f[ti.i32(coord.x), ti.i32(coord.y)]
+    return val
 
 @ti.kernel
-def Jacobi(fh : ti.template(), vh : ti.template(), vvh : ti.template(), h2 : ti.f32): # fh = solution field, vh = old approximation value (being read), vvh = new approximation value, h2 = cell width squared  
+def Jacobi(fh : ti.template(), vh : ti.template(), vvh : ti.template(), h2 : ti.f32, residualField : ti.template()): # fh = solution field, vh = old approximation value (being read), vvh = new approximation value, h2 = cell width squared  
     for i, j in vvh:  # Parallelized over all pixels
         s = ( SampleField(vh, tm.vec2(i-1,j) ) + SampleField(vh, tm.vec2(i+1,j) ) + SampleField(vh, tm.vec2(i,j-1) ) + SampleField(vh, tm.vec2(i,j+1) ) - h2 * SampleField(fh, tm.vec2(i,j)) ) / 4.
         vvh[i,j] = s
+        residualField[i,j] = ti.abs(vh[i,j] - vvh[i,j])
 
-# @ti.kernel
-# def Residual( fh, vh, rh):
-#     for i, j in fh:  # Parallelized over all pixels
-#         rh[i,j] = - SampleField(fh, (i,j)) - ()
+#@ti.kernel
+def ComputeResidual( residualField : ti.template() ) -> ti.f32: #todo: parallel reduce add
+    sum = 0
+    for i,j in residualField:
+        sum += residualField[i,j]
+    return sum
+
+
+AbsVField = ti.Vector.field(2, ti.f32, shape=(dim, dim)) 
+AbsFHField = ti.Vector.field(2, ti.f32, shape=(dim, dim))  
+@ti.kernel
+def AbsVals(vField : ti.template(), fhField : ti.template()):
+    for i,j in vField:
+        AbsVField[i,j].x = (vField[i,j]) * 100 #Positive values
+        AbsVField[i,j].y = -(vField[i,j]) * 100 #Negative Values
+    for i,j in fhField:
+        AbsFHField[i,j].x = (fhField[i,j]) * 10 #Positive values
+        AbsFHField[i,j].y = -(fhField[i,j]) * 10 #Negative Values
+
 
 ###############################################################################################
 # Main Program
 ###############################################################################################
 Init()
 PrevFrameCursorPos = tm.vec2(0.0,0.0) 
-gui = ti.GUI("Jacobi Solver", res=(DimLvl1, DimLvl1))
+gui = ti.GUI("Jacobi Solver", res=(dim, dim))
 
 # Au = f        # A is a sparse matrix .  u is the unknown vector. and f is the right hand side of the equation (ie: the solution) 
 # u = A^-1 · f  # Invert of A multiplied by f equals the unknown u. But inverse of A is complex to calculate & takes a lot of memory.
 # vα = approximation of unknown "u", at iteration α, which converges to A^-1 · f
 
-## Example:
-# Original matrix A:
-# 2 1 0 0
-# 1 2 1 0
-# 0 1 2 1
-# 0 0 1 2
-
-#Inverse of matrix A:
-# 4/5 -3/5 2/5 -1/5
-# -3/5 6/5 -4/5 2/5
-# 2/5 -4/5 6/5 -3/5
-# -1/5 2/5 -3/5 4/5
-
-# unknown U:
-# 1 2 3 4
-# 5 6 7 8
-# 9 10 11 12
-# 13 14 15 16
-
-# solution f: 
-# 7 10 13 16
-# 20 24 28 32
-# 36 40 44 48
-# 35 38 41 44
-
 ##############################
 ## Solver
 ##############################
-CellSize = 1. #/ DimLvl1
+CellSize = 1. /  dim
+CellSizeSqr = CellSize * CellSize
 
 ping = True
-IterationCount = 60
-for i in range(IterationCount):
+residualThreshold = 1e-4
+i = 0
+while True:
+    i+=1
     if ping:
-        Jacobi(FHField, VHField_Ping, VHField_Pong, CellSize )
+        Jacobi(FHField, VHField_Pong, VHField_Ping, CellSizeSqr, ResidualField )
     else:
-        Jacobi(FHField, VHField_Pong, VHField_Ping, CellSize )
+        Jacobi(FHField, VHField_Ping, VHField_Pong, CellSizeSqr, ResidualField )
 
     ping = not ping
 
+    numpResidualArray = ResidualField.to_numpy()
+    residualSum = numpResidualArray.sum()
+    if residualSum  < residualThreshold:
+        print("performed ", i, " jacobi iterations before reaching residual threshold")
+        break
+
 ping = not ping #flip back to last written ping/pong 
 
+AbsVals(VHField_Ping if ping else VHField_Pong, FHField)
+DisplayedBuffer = 2
 while gui.running:
 
 
     ##############################
     ## Render
     ##############################
-    gui.set_image(VHField_Ping if ping else VHField_Pong)
+    if gui.get_event(ti.GUI.RELEASE, ti.GUI.PRESS):
+        if gui.event.type == ti.GUI.RELEASE:
+            if gui.event.key == ti.GUI.ESCAPE:
+                gui.close()
+                break
+            if gui.event.key == ti.GUI.SPACE:
+                Reset()
+            if  gui.event.key == 'a':
+                DisplayedBuffer = 1
+            if gui.event.key == 'z':
+                DisplayedBuffer = 2
+
+    if DisplayedBuffer == 1:
+        gui.set_image(AbsVField)
+    elif DisplayedBuffer == 2:
+        gui.set_image(AbsFHField)
+
     gui.show()

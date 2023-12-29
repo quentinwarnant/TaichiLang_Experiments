4c9b14ac8bb213cc2059ec15d20dba7ba7e85f8f
Quentin Warnant <lequentin@gmail.com>
2023-12-29 14:04:06 +0000

    Trying to implement Gauss Seidel Red Black, isn't converging yet

--- FluidSim/PoissonSolvers/JacobiSolver.py
+++ FluidSim/PoissonSolvers/JacobiSolver.py
@@ -1,11 +1,22 @@
 import taichi as ti
 import taichi.math as tm
+from enum import Enum
+import math
+import sys
+import numpy
+numpy.set_printoptions(threshold=sys.maxsize)
 
 _DEBUG = False
 
 # Configuration
-dim = 256
+dim = 64
 dimSqr = dim * dim
+
+class Solver(Enum):
+    Jacobi = 1
+    GaussSeidelRB = 2
+
+ActiveSolver = Solver.GaussSeidelRB
 
 # Taichi Initialisation
 if _DEBUG:
@@ -29,12 +40,23 @@ def Reset():
     FHField.fill(0.)
 
     #b aka F aka fh
+    # for i in range(0,dim):
+    #     FHField[i,0] = 60.
+    # for i in range (60, dim-60):   
+    #     for j in range(0,10):
+    #         FHField[i,dim-2-j] = -15.
+    #         FHField[j,i] = -15.
+
     for i in range(0,dim):
-        FHField[i,0] = 60.
-    for i in range (60, dim-60):   
-        for j in range(0,10):
+        FHField[i,3] = 60.
+    for i in range (3, dim-3):   
+        for j in range(0,13):
             FHField[i,dim-2-j] = -15.
-            FHField[j,i] = -15.
+            FHField[j,1+i] = -15.
+
+
+
+
 
     # for x,y in FHField:
     #     FHField[x,y] = -5. * tm.exp(x) * tm.exp(-2. * y)
@@ -49,7 +71,6 @@ def Init():
 @ti.func
 def SampleField(f, coord : tm.vec2) -> ti.f32:
     extents = tm.vec2(f.shape[0]-1, f.shape[1]-1)
-    #coord = tm.clamp(coord, tm.vec2(0,0), extents)
     val = 0.
     if coord.x < 0 or coord.x >= extents.x or coord.y < 0 or coord.y >= extents.y:
         val = 0.
@@ -71,40 +92,39 @@ def OneDCoordToTwoD(coord : ti.i32) -> tm.ivec2:
 def OffsetTwoDCoord(coord : tm.ivec2, offset : ti.i32) -> tm.ivec2:
     return OneDCoordToTwoD( TwoDCoordToOneD(coord) + offset)
 
-@ti.func
-def ComputeResidual(GridIndexX, GridIndexY, currentXGuess : ti.f32, b : ti.template()) -> ti.f32:
-    #construct A row which represents
-    LinearGridIndex = (GridIndexY * dim) + GridIndexX
-    # Matrix A coeficients for this linear system of equations line
-    coefLeftMostDiagonal = 0 if GridIndexY < 3 else 1
-    coefOffDiagonalLeft = 0 if GridIndexY < 1 else 1
-    coefDiagonal = -4
-    coefOffDiagonalRight = 0 if GridIndexY < (dim - 2) else 1
-    coefRightMostDiagonal = 0 if GridIndexY < (dim - 4) else 1
-
-    coord = tm.ivec2(GridIndexX, GridIndexY)
-    total = b[coord] * coefDiagonal
-    if coefLeftMostDiagonal != 0:
-        total += b[OffsetTwoDCoord(coord,-3)] * coefLeftMostDiagonal
-    if coefOffDiagonalLeft != 0:
-        total += b[OffsetTwoDCoord(coord,-1)] * coefOffDiagonalLeft
-    if coefOffDiagonalRight != 0:
-        total += b[OffsetTwoDCoord(coord,1)] * coefOffDiagonalRight
-    if coefRightMostDiagonal != 0:
-        total += b[OffsetTwoDCoord(coord,3)] * coefRightMostDiagonal
-
-    #if abs(currentXGuess - total) > 0:
-    #    print("residual[",GridIndexX,",",GridIndexY,"] = ", currentXGuess - total)
-    return ti.abs(currentXGuess - total)
-
 @ti.kernel
 def Jacobi(fh : ti.template(), vh : ti.template(), vvh : ti.template(), h2 : ti.f32, residualField : ti.template()): # fh = solution field, vh = old approximation value (being read), vvh = new approximation value, h2 = cell width squared  
     for i, j in vvh:  # Parallelized over all pixels
-        s = ( SampleField(vh, tm.vec2(i-1,j) ) + SampleField(vh, tm.vec2(i+1,j) ) + SampleField(vh, tm.vec2(i,j-1) ) + SampleField(vh, tm.vec2(i,j+1) ) - h2 * SampleField(fh, tm.vec2(i,j)) ) / 4.
-        vvh[i,j] = s
-        residualField[i,j] = ti.abs(vh[i,j] - vvh[i,j])
-        #residualField[i,j] = ComputeResidual(i,j, s, fh) #not working correctly...
+        if i != 0 and i != dim-1 and j != 0 and j != dim-1:
+            s = ( SampleField(vh, tm.vec2(i-1,j) ) + SampleField(vh, tm.vec2(i+1,j) ) + SampleField(vh, tm.vec2(i,j-1) ) + SampleField(vh, tm.vec2(i,j+1) ) - h2 * SampleField(fh, tm.vec2(i,j)) ) / 4.
+            vvh[i,j] = s
+            residualField[i,j] = tm.pow(vh[i,j] - vvh[i,j], 2.)
 
+@ti.kernel
+def GaussSeidel(fh : ti.template(), vh : ti.template(), vvh : ti.template(), h2 : ti.f32, residualField : ti.template(), red : ti.i8): # fh = solution field, vh = old approximation value (being read), vvh = new approximation value, h2 = cell width squared  
+    halfDim = dim/2
+    halfGrid = dim * halfDim
+    for index in range(halfGrid): # Parallelized
+        j = tm.floor(index/halfDim, ti.i32)
+        tmpI = ti.i32(index - (j * halfDim))
+
+        offset = 0
+        jIsEven = (j % 2 == 0)
+        if red == 1:
+            offset = 0 if jIsEven else 1
+        else:
+            offset = 1 if jIsEven else 0
+
+        i = (tmpI * 2) + offset
+
+        
+        # not working properly yet...
+        if i != 0 and i != dim-1 and j != 0 and j != dim-1:
+            s = ( SampleField(vh, tm.vec2(i-1,j) ) + SampleField(vh, tm.vec2(i+1,j) ) + SampleField(vh, tm.vec2(i,j-1) ) + SampleField(vh, tm.vec2(i,j+1) ) - h2 * SampleField(fh, tm.vec2(i,j)) ) / 4.
+
+
+            vvh[i,j] = s * 0.5
+            residualField[i,j] = tm.pow(vh[i,j] - vvh[i,j], 2.)
 
 AbsVField = ti.Vector.field(2, ti.f32, shape=(dim, dim)) 
 AbsFHField = ti.Vector.field(2, ti.f32, shape=(dim, dim))  
@@ -123,7 +143,7 @@ def AbsVals(vField : ti.template(), fhField : ti.template()):
 ###############################################################################################
 Init()
 PrevFrameCursorPos = tm.vec2(0.0,0.0) 
-gui = ti.GUI("Jacobi Solver", res=(dim, dim))
+gui = ti.GUI("Poisson Equation Solver", res=(dim, dim))
 
 # Au = f        # A is a sparse matrix .  u is the unknown vector. and f is the right hand side of the equation (ie: the solution) 
 # u = A^-1 · f  # Invert of A multiplied by f equals the unknown u. But inverse of A is complex to calculate & takes a lot of memory.
@@ -136,26 +156,34 @@ CellSize = 1. /  dim
 CellSizeSqr = CellSize * CellSize
 
 ping = True
-residualThreshold = 1e-1
+residualThreshold = 1e-6
 i = 0
 while True:
     i+=1
-    if ping:
-        Jacobi(FHField, VHField_Pong, VHField_Ping, CellSizeSqr, ResidualField )
-    else:
-        Jacobi(FHField, VHField_Ping, VHField_Pong, CellSizeSqr, ResidualField )
+    if ActiveSolver == Solver.Jacobi:
+        if ping:
+            Jacobi(FHField, VHField_Pong, VHField_Ping, CellSizeSqr, ResidualField )
+        else:
+            Jacobi(FHField, VHField_Ping, VHField_Pong, CellSizeSqr, ResidualField )
+    elif ActiveSolver == Solver.GaussSeidelRB:
+            GaussSeidel(FHField, VHField_Ping, VHField_Pong, CellSizeSqr, ResidualField, 1 )
+            GaussSeidel(FHField, VHField_Pong, VHField_Ping, CellSizeSqr, ResidualField, 0 )
 
     ping = not ping
 
     numpResidualArray = ResidualField.to_numpy()
-    residualSum = numpResidualArray.sum()
-    if residualSum  < residualThreshold:
-        print("performed ", i, " jacobi iterations before reaching residual threshold")
+    residualSum = numpResidualArray.sum() 
+    residual = math.sqrt(residualSum) / math.sqrt(dimSqr)
+    if residual  < residualThreshold: # use L2Norm(residual, fine.size) // sqrt(sum(r*r))
+        print("performed ", i, " iterations before reaching residual threshold")
         break
 
 ping = not ping #flip back to last written ping/pong 
+if ActiveSolver == Solver.Jacobi:
+    AbsVals(VHField_Ping if ping else VHField_Pong, FHField)
+elif ActiveSolver == Solver.GaussSeidelRB:
+    AbsVals(VHField_Ping, FHField)
 
-AbsVals(VHField_Ping if ping else VHField_Pong, FHField)
 DisplayedBuffer = 2
 
 while gui.running:
